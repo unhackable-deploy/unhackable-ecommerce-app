769e45dd35b415f9ba5a0e08be5a76baf2fbeec5
Gabe O'Brien <gabe.obrien@lacework.net>
2022-09-21 12:47:35 -0700

    feat: ecomm order service can now call out to an API gateway that invokes a Lamdba functon

---
+++ lambda/index.js
@@ -0,0 +1,26 @@
+var AWS = require('aws-sdk');
+
+async function putObjectToS3(bucket, key, data){
+    var s3 = new AWS.S3();
+    var params = {
+        Bucket : bucket,
+        Key : key,
+        Body : data
+    }
+    await s3.putObject(params, function(err, data) {
+        if (err) console.log(err, err.stack);
+        else     console.log(data);
+    }).promise();;
+}
+
+exports.handler = async (event) => {
+    const rand = (Math.random() + 1).toString(36).substring(7);
+    const file_name = "order_"+rand+".csv"
+    const file_contents = "id\tname\tdata\n"+rand+"\tAA\ttoday"
+    await putObjectToS3(process.env.BUCKET_NAME, file_name, file_contents)
+    const response = {
+        statusCode: 200,
+        body: '{ "status": "orderPlaced" }'
+    };
+    return response;
+};
